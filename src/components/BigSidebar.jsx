8ba277375e2f1fb63c376c3eea6512b72051474f
Messiri4 <messirimiracle@gmail.com>
2024-06-16 10:26:44 +0100

    DarkTheme

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,6 +1,26 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import NavLinks from "./NavLinks";
+import Logo from "./Logo";
+import { useDashboardContext } from "../pages/DashboardLayout";
 
 const BigSidebar = () => {
-  return <Wrapper>BigSideBar</Wrapper>;
+  const { showSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default BigSidebar;
